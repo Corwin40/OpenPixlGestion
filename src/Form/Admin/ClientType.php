ea95d8887207d4fcbe5032fc1cbf28a8cd3d8dab
Xavier BURKE <xavier.burke@gmail.com>
2021-04-06 17:18:31 +0200

    CRUD Client

---
+++ src/Form/Admin/ClientType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\Admin\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('lastName')
+            ->add('firstName')
+            ->add('address')
+            ->add('city')
+            ->add('zipcode')
+            ->add('email')
+            ->add('phoneDesk')
+            ->add('createdAt')
+            ->add('updatedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
