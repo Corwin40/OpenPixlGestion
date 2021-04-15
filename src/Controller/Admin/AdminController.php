e605c0bb055c469218946af345c3f759b6943cd3
Guillaume <guillaume.jsn57@gmail.com>
2021-04-15 11:52:54 +0200

    update dashboard

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Admin\Client;
+use App\Entity\Admin\Server;
 use App\Entity\Admin\Service;
 use App\Entity\Webapp\Facture;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,12 +29,15 @@ class AdminController extends AbstractController
         $clients = $this->getDoctrine()->getRepository(Client::class)->findAll();
         // Je récupère la liste des servies depuis l'entité service
         $services = $this->getDoctrine()->getRepository(Service::class)->findAll();
+        // Je récupère la liste des serveurs depuis l'entité server
+        $servers = $this->getDoctrine()->getRepository(Server::class)->findAll();
         // Je récupère la liste des facture depuis l'entité facture
         $factures = $this->getDoctrine()->getRepository(Facture::class)->findAll();
 
         return $this->render('admin/admin/dashboard.html.twig',[
             'clients' => $clients,
             'services' => $services,
+            'servers' => $servers,
             'factures' => $factures
         ]);
     }
